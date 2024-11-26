6541eff401b02527a96cbc9de77ea56cfb8838d3
anaGianno <blcoronado2004@gmail.com>
2024-11-26 18:06:30 +1300

    add routes for track table

--- backend/server.js
+++ backend/server.js
@@ -187,25 +187,7 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-app.get("/tracks", async (req, res) => {
-  try {
-    const allTracks = await pool.query("SELECT * FROM track");
-    res.send(allTracks.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-// app.post("/tracks", async (req, res) => {
-//   try {
-//     const{id,name,} = req.body;
-//     const newTrack = await pool.query("INSERT INTO track (description) VALUES($1) RETURNING *",[description]);
-//     res.json(newTrack.rows[0]);
-//   } catch (err) {
-//     console.error(err.message);
-//   }
-// });
-
+//user routes
 app.get("/users", async (req, res) => {
   try {
     const allUsers = await database.query("SELECT * FROM user_");
@@ -253,12 +235,64 @@ app.delete("/users/:id", async (req, res) => {
   }
 });
 
-//ensure a username has been entered
-function validateUser(user) {
-  const schema = Joi.object({
-    // email: Joi.string().required(),
-    allowUnknown: true,
-  });
+//track routes
+app.get("/tracks", async (req, res) => {
+  try {
+    const allTracks = await database.query("SELECT * FROM track");
+    res.send(allTracks.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
-  return schema.validate(user);
+app.post("/tracks", async (req, res) => {
+  try {
+    const { track_id, track_name, artist_name, image_url, track_user_id } =
+      req.body;
+    var { duration } = req.body;
+    duration = millisToMinutesAndSeconds(duration);
+    const newTrack = await database.query(
+      "INSERT INTO track (track_id,track_name,artist_name,duration,image_url,track_user_id) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",
+      [track_id, track_name, artist_name, duration, image_url, track_user_id]
+    );
+    res.json(newTrack.rows[0]);
+  } catch (err) {
+    // //pass the error to middleware
+    // next(error);
+    console.error("Console Error: " + err.message);
+  }
+});
+
+app.delete("/tracks/:id", async (req, res) => {
+  try {
+    const track = await database.query(
+      "SELECT * FROM track WHERE track_id = $1",
+      [req.params.id]
+    );
+    if (track.rows[0] == undefined)
+      return res.status(404).send("The track with the given ID was not found");
+    const updateUser = await database.query(
+      "DELETE FROM track WHERE track_id = $1",
+      [req.params.id]
+    );
+    res.send(`Track with ID: ${req.params.id} was deleted`);
+  } catch (err) {
+    console.error("Console Error: " + err.message);
+  }
+});
+
+// //ensure a username has been entered
+// function validateUser(user) {
+//   const schema = Joi.object({
+//     // email: Joi.string().required(),
+//     allowUnknown: true,
+//   });
+
+//   return schema.validate(user);
+// }
+
+function millisToMinutesAndSeconds(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
 }
