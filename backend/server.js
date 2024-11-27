3c2a8cf67734ff75abdc98232bebc26774136d4f
anaGianno <blcoronado2004@gmail.com>
2024-11-27 13:59:33 +1300

    add routes for artist table

--- backend/server.js
+++ backend/server.js
@@ -281,6 +281,48 @@ app.delete("/tracks/:id", async (req, res) => {
   }
 });
 
+//artist routes
+app.get("/artists", async (req, res) => {
+  try {
+    const allArtists = await database.query("SELECT * FROM artist");
+    res.send(allArtists.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.post("/artists", async (req, res) => {
+  try {
+    const { artist_id, artist_name, image_url, artist_user_id } = req.body;
+    const newArtist = await database.query(
+      "INSERT INTO artist (artist_id,artist_name,image_url,artist_user_id) VALUES($1,$2,$3,$4) RETURNING *",
+      [artist_id, artist_name, image_url, artist_user_id]
+    );
+    res.json(newArtist.rows[0]);
+  } catch (err) {
+    // //pass the error to middleware
+    // next(error);
+    console.error("Console Error: " + err.message);
+  }
+});
+
+app.delete("/artists/:id", async (req, res) => {
+  try {
+    const artist = await database.query(
+      "SELECT * FROM artist WHERE artist_id = $1",
+      [req.params.id]
+    );
+    if (artist.rows[0] == undefined)
+      return res.status(404).send("The artist with the given ID was not found");
+    const updateArtist = await database.query(
+      "DELETE FROM artist WHERE artist_id = $1",
+      [req.params.id]
+    );
+    res.send(`Artist with ID: ${req.params.id} was deleted`);
+  } catch (err) {
+    console.error("Console Error: " + err.message);
+  }
+});
 // //ensure a username has been entered
 // function validateUser(user) {
 //   const schema = Joi.object({
