e9ca724bb3d65adf1cde45b6be238d5ed0def08c
anaGianno <blcoronado2004@gmail.com>
2024-11-27 14:59:13 +1300

    add routes for album table

--- backend/server.js
+++ backend/server.js
@@ -323,6 +323,65 @@ app.delete("/artists/:id", async (req, res) => {
     console.error("Console Error: " + err.message);
   }
 });
+
+//album routes
+app.get("/albums", async (req, res) => {
+  try {
+    const allAlbums = await database.query("SELECT * FROM album");
+    res.send(allAlbums.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.post("/albums", async (req, res) => {
+  try {
+    const {
+      album_id,
+      album_name,
+      album_release_date,
+      total_tracks,
+      image_url,
+      artist_name,
+      album_user_id,
+    } = req.body;
+    const newAlbum = await database.query(
+      "INSERT INTO album (album_id,album_name,album_release_date,total_tracks,image_url,artist_name,album_user_id) VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING *",
+      [
+        album_id,
+        album_name,
+        album_release_date,
+        total_tracks,
+        image_url,
+        artist_name,
+        album_user_id,
+      ]
+    );
+    res.json(newAlbum.rows[0]);
+  } catch (err) {
+    // //pass the error to middleware
+    // next(error);
+    console.error("Console Error: " + err.message);
+  }
+});
+
+app.delete("/albums/:id", async (req, res) => {
+  try {
+    const album = await database.query(
+      "SELECT * FROM album WHERE album_id = $1",
+      [req.params.id]
+    );
+    if (album.rows[0] == undefined)
+      return res.status(404).send("The album with the given ID was not found");
+    const updateAlbum = await database.query(
+      "DELETE FROM album WHERE album_id = $1",
+      [req.params.id]
+    );
+    res.send(`Album with ID: ${req.params.id} was deleted`);
+  } catch (err) {
+    console.error("Console Error: " + err.message);
+  }
+});
 // //ensure a username has been entered
 // function validateUser(user) {
 //   const schema = Joi.object({
